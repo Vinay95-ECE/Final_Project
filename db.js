16460f491e80747e9e9a70a23fe20456aaa97edd
Vinay95-ECE <vvbrother360@gmail.com>
2026-01-02 22:47:10 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,1 +1,8 @@
-//database is here
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB Connected");
+};
+
+module.exports = connectDB;
