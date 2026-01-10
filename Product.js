841615a67d5e0b80232f5336ade889b71d1e10bb
Vinay95-ECE <vvbrother360@gmail.com>
2026-01-10 23:24:09 +0530

    Update Product.js

--- Product.js
+++ Product.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  type: String, // seed | fertilizer
+  type: String, // seed | fertilizer section
   name: String,
   price: Number,
   description: String
 });
+
 
 module.exports = mongoose.model("Product", productSchema);
